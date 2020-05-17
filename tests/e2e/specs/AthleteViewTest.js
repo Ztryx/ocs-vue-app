08ee3935d688a59ab10155d6aaddb0a84dd2aabe
Andrés Jesús Díaz Santos <not.andres.diaz@gmail.com>
2020-05-17 22:42:59 +0200

    Add testing with mocha and cypress

--- tests/e2e/specs/AthleteViewTest.js
+++ tests/e2e/specs/AthleteViewTest.js
@@ -1,17 +1,18 @@
-describe('Athletes View Test', () => {
-  it('Retrieves elements from API', () => {
+describe('Athlete View Test', () => {
+  it('View for Athlete is working', () => {
     cy.visit('/athletes');
-    cy.contains('h1', 'Olympic Athletes');
-    cy.contains('h2', 'Pyeongchang 2018');
-    cy.contains('h2', 'Rio de Janeiro 2016');
+    cy.get('div.athlete-bottom-card').first().click();
+    cy.contains('h1', 'Athlete Arianna Fontana');
+    cy.contains('p', 'Pyeongchang 2018');
+    cy.contains('p', 'Sochi 2014');
+    cy.contains('p', 'Vancouver 2010');
+    cy.contains('p', 'Turin 2006');
   });
-  it('Retrieves elements from API with Athletes per Game', () => {
+  it('Markdown conversion is working', () => {
     cy.visit('/athletes');
-    cy.contains('span', 'Michael Phelps');
-  });
-  it('More button pagination works', () => {
-    cy.visit('/athletes');
-    cy.get('.more-container button').click();
-    cy.contains('h2', 'Atlanta 1996');
+    cy.get('div.athlete-bottom-card').first().click();
+    cy.contains('h1', 'Athlete Arianna Fontana');
+    cy.contains('p', 'Fontana started skating at the age of four, following in the footsteps of her brother Alessandro, ' +
+      'initially on roller skates before switching to ice skating. She first trained in Lanzada until the rink there closed, when she switched her training base to Bormio.');
   });
 });
